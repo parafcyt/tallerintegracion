26ff7bf85f2285e834a06ee6e5b6fc62773215c0
parafcyt <parafcyt@gmail.com>
2020-03-08 22:53:30 -0300

    sql creacion de tablas

---
+++ BBDD/01 Creacion de tablas/tablas.sql
@@ -0,0 +1,78 @@
+CREATE TABLE usuario(
+    id serial PRIMARY KEY,
+    nombrereal varchar(40) not null,
+    nombreusuario varchar(40) not null,
+    contrasenia varchar(40) not null,
+    permisousuario varchar(40) not null --base, administrador,administrativo
+);
+
+CREATE TABLE examinador(
+    id serial PRIMARY KEY,
+    activo boolean,
+    nombrereal varchar(40) not null,
+    idusuario int references usuario(id)
+);
+
+CREATE TABLE verificador(
+    id serial PRIMARY KEY,
+    activo boolean,
+    nombrereal varchar(40) not null,
+    idusuario int references usuario(id)
+);
+
+CREATE TABLE conductor(
+    dni int PRIMARY KEY,
+    nombre varchar(40) not null,
+    apellido varchar(40) not null
+);
+
+CREATE TABLE dominio(
+    id varchar(30) PRIMARY KEY,
+    descripcion varchar(100) not null
+);
+
+CREATE TABLE equipo(
+    id serial PRIMARY KEY,
+    nombre varchar(40) not null,
+    activo boolean default false,
+    nroactual int not null
+);
+
+CREATE TABLE periodoutilizable(
+    id serial PRIMARY KEY,
+    activo boolean,
+    fechainicio date not null,
+    fechavencimiento date not null,
+    nroingreso int not null,
+    idequipo int references equipo(id)
+);
+
+CREATE TABLE prestamo(
+    id serial PRIMARY KEY, 
+    activo boolean,
+    fechaprestamo date not null,
+    horaprestamo time not null,
+    nroinicial int not null,
+    fechadevolucion date,
+    horadevolucion time,
+    nrodevolucion int,
+    idexaminador int references examinador(id),
+    idequipo int references equipo(id)
+);
+
+CREATE TABLE prueba(
+    id serial PRIMARY KEY,
+    fecha date not null,
+    hora time not null,
+    nromuestra int not null,
+    resultado float not null check(resultado>=0.0),
+    nroacta int default 0,
+    nroretencion int default 0,
+    verificado boolean default false,
+    rechazado boolean,
+    descripcionrechazo varchar(100),
+    idverificador int references verificador(id), 
+    idconductor int references conductor(dni)
+    iddominio varchar references dominio(id),
+    idprestamo int references prestamo(id)
+);
